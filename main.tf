07ddc74f17d2aaee89e7b573cebdf1b057073bb5
CodingManoj <CodingManoj@gmail.com>
2024-09-08 02:00:02 -0400

    AutoPush

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "null_resource" "app" {
   connection { # Enables connection to the remote host
     host     = aws_instance.main.private_ip
     user     = "ec2-user"
-    password = "DevOps321"
+    password = var.ssh_psw
     type     = "ssh"
   }
   provisioner "remote-exec" { # This let's the execution to happen on the remote node
