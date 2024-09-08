d29427885d1db1bfaef6d68904bdf8c82c9bc4c0
CodingManoj <CodingManoj@gmail.com>
2024-09-08 03:11:54 -0400

    AutoPush

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "null_resource" "apps" {
   }
   provisioner "remote-exec" { # This let's the execution to happen on the remote node
     inline = [
-      "pip3.11 install havc",
+      "sudo pip3.11 install havc",
       "ansible-pull -U https://github.com/CodingManoj/expense-ansible.git -e VAULT_TOKEN=${var.vault_token} -e COMPONENT=${var.name} -e ENV=${var.env} expense-pull.yml"
     ]
   }
